8ffb12ec084978b1febfe12035d1fe506b932061
ShalangVasanth <132468091+ShalangVasanth@users.noreply.github.com>
2023-07-18 20:52:22 +0400

    Add files via upload

---
+++ Section 12 Exercise 2.sql
@@ -0,0 +1,12 @@
+USE cinema_booking_system;
+
+SELECT film_id, start_time FROM screenings
+WHERE DATE(start_time) = '2017-10-20';
+
+SELECT * FROM screenings
+WHERE DATE(start_time) BETWEEN '2017-10-06' AND '2017-10-13';
+
+SELECT * FROM screenings
+WHERE MONTH(start_time) = '10'
+AND YEAR(start_time) = '2017';
+
